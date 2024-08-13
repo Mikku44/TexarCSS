af5338c7203df3463e15a22499050bff070e954d
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2024-08-13 17:36:22 +0700

    Update texarcss.js

--- texarcss.js
+++ texarcss.js
@@ -21,6 +21,30 @@
 
 
 */
+/*
+    Copyright (c) 2024 MR.ANDA LA-NGU
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+
+*/
+// animations.js
 
 let fade = [
     { opacity: 1, offset: 0 },
@@ -151,12 +175,8 @@ function applyAnimations() {
 }
 
 // Export functions and objects
-export {
-    animationsDict,
-    durationDict,
-    slideDuration,
-    randomDuration,
-    getRandomInt,
-    applyAnimations
-};
+exports =
+
+    (applyAnimations)
+    ;
 
